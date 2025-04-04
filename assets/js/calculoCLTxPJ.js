b5933b6d389e59a4c8aa9d51988daaf8bd44b4f6
Ricardo Rodrigues <ricardo272530@gmail.com>
2025-04-04 11:43:35 -0300

    melhorias no codigo 'geral'

--- assets/js/calculoCLTxPJ.js
+++ assets/js/calculoCLTxPJ.js
@@ -30,37 +30,46 @@ function calcularCLTxPJ() {
     let feriasMensal = (salarioBruto / 12) * 1.3333;
     let decimoTerceiroMensal = salarioBruto / 12;
 
-    let inss = 0;
+    function calcularINSS() {
+        let inss = 0;
 
-    if (salarioBruto <= 1412.00) {
-        inss = salarioBruto * 0.075;
-    } else if (salarioBruto <= 2666.68) {
-        inss = (1412 * 0.075) + ((salarioBruto - 1412) * 0.09);
-    } else if (salarioBruto <= 4000.03) {
-        inss = (1412 * 0.075) + ((2666.68 - 1412) * 0.09) + ((salarioBruto - 2666.68) * 0.12);
-    } else if (salarioBruto <= 7786.02) {
-        inss = (1412 * 0.075) + ((2666.68 - 1412) * 0.09) + ((4000.03 - 2666.68) * 0.12) + ((salarioBruto - 4000.03) * 0.14);
-    } else {
-        inss = 908.86;
+        if (salarioBruto <= 1412.00) {
+            inss = salarioBruto * 0.075;
+        } else if (salarioBruto <= 2666.68) {
+            inss = (1412 * 0.075) + ((salarioBruto - 1412) * 0.09);
+        } else if (salarioBruto <= 4000.03) {
+            inss = (1412 * 0.075) + ((2666.68 - 1412) * 0.09) + ((salarioBruto - 2666.68) * 0.12);
+        } else if (salarioBruto <= 7786.02) {
+            inss = (1412 * 0.075) + ((2666.68 - 1412) * 0.09) + ((4000.03 - 2666.68) * 0.12) + ((salarioBruto - 4000.03) * 0.14);
+        } else {
+            inss = 908.86;
+        }
+
+        return inss;
     }
 
-    let baseIRPF = salarioBruto - inss;
-    let irpf = 0;
+    function calcularIRPF() {
+        let inss = calcularINSS();
+        let baseIRPF = salarioBruto - inss;
+        let irpf = 0;
 
-    if (baseIRPF <= 2259.20) {
-        irpf = 0;
-    } else if (baseIRPF <= 2826.65) {
-        irpf = (baseIRPF * 0.075) - 169.44;
-    } else if (baseIRPF <= 3751.05) {
-        irpf = (baseIRPF * 0.15) - 381.44;
-    } else if (baseIRPF <= 4664.68) {
-        irpf = (baseIRPF * 0.225) - 662.77;
-    } else {
-        irpf = (baseIRPF * 0.275) - 896.00;
+        if (baseIRPF <= 2259.20) {
+            irpf = 0;
+        } else if (baseIRPF <= 2826.65) {
+            irpf = (baseIRPF * 0.075) - 169.44;
+        } else if (baseIRPF <= 3751.05) {
+            irpf = (baseIRPF * 0.15) - 381.44;
+        } else if (baseIRPF <= 4664.68) {
+            irpf = (baseIRPF * 0.225) - 662.77;
+        } else {
+            irpf = (baseIRPF * 0.275) - 896.00;
+        }
+
+        return irpf;
     }
 
-    let salarioFinal = (salarioBruto + valeAlimentacao + valeTransporte + beneficios + feriasMensal + decimoTerceiroMensal) - inss - irpf;
-    let salarioLiquido = (salarioBruto + valeAlimentacao + valeTransporte + beneficios) - inss - irpf;
+    let salarioFinal = (salarioBruto + valeAlimentacao + valeTransporte + beneficios + feriasMensal + decimoTerceiroMensal) - calcularINSS() - calcularIRPF();
+    let salarioLiquido = (salarioBruto + valeAlimentacao + valeTransporte + beneficios) - calcularINSS() - calcularIRPF();
 
     aliquotaPJ = 0.15;
     let salarioPJ = aliquotaPJ > 0 ? (salarioFinal / (1 - aliquotaPJ)) : salarioFinal;
@@ -72,8 +81,8 @@ function calcularCLTxPJ() {
     document.getElementById("vFeriasMensal").innerHTML = `R$ ${feriasMensal.toFixed(2)}`;
     document.getElementById("vDecimoTerceiroMensal").innerHTML = `R$ ${decimoTerceiroMensal.toFixed(2)}`;
 
-    document.getElementById("dINSS").innerHTML = `-R$ ${inss.toFixed(2)}`;
-    document.getElementById("dIRPF").innerHTML = `-R$ ${irpf.toFixed(2)}`;
+    document.getElementById("dINSS").innerHTML = `-R$ ${calcularINSS().toFixed(2)}`;
+    document.getElementById("dIRPF").innerHTML = `-R$ ${calcularIRPF().toFixed(2)}`;
     document.getElementById("dValeTransporte").innerHTML = `-R$ ${valeTransporte.toFixed(2)}`;
 
     document.getElementById("tSalarioFinal").innerHTML = `R$ ${salarioFinal.toFixed(2)}`;
--- assets/js/calculoFerias.js
+++ assets/js/calculoFerias.js
@@ -5,34 +5,44 @@ function calcularFerias() {
     let feriasMensal = (salarioBrutoFerias * 1.3333) / 12;
     let ferias = feriasMensal * mesesTrabalhado;
 
-    let inss = 0;
+    function calcularINSS() {
 
-    if (salarioBrutoFerias <= 1412.00) {
-        inss = salarioBrutoFerias * 0.075;
-    } else if (salarioBrutoFerias <= 2666.68) {
-        inss = (1412 * 0.075) + ((salarioBrutoFerias - 1412) * 0.09);
-    } else if (salarioBrutoFerias <= 4000.03) {
-        inss = (1412 * 0.075) + ((2666.68 - 1412) * 0.09) + ((salarioBrutoFerias - 2666.68) * 0.12);
-    } else if (salarioBrutoFerias <= 7786.02) {
-        inss = (1412 * 0.075) + ((2666.68 - 1412) * 0.09) + ((4000.03 - 2666.68) * 0.12) + ((salarioBrutoFerias - 4000.03) * 0.14);
-    } else {
-        inss = 908.86;
+        let inss = 0;
+
+        if (salarioBrutoFerias <= 1412.00) {
+            inss = salarioBrutoFerias * 0.075;
+        } else if (salarioBrutoFerias <= 2666.68) {
+            inss = (1412 * 0.075) + ((salarioBrutoFerias - 1412) * 0.09);
+        } else if (salarioBrutoFerias <= 4000.03) {
+            inss = (1412 * 0.075) + ((2666.68 - 1412) * 0.09) + ((salarioBrutoFerias - 2666.68) * 0.12);
+        } else if (salarioBrutoFerias <= 7786.02) {
+            inss = (1412 * 0.075) + ((2666.68 - 1412) * 0.09) + ((4000.03 - 2666.68) * 0.12) + ((salarioBrutoFerias - 4000.03) * 0.14);
+        } else {
+            inss = 908.86;
+        }
+
+        return inss;
     }
-    
 
-    let baseIRPF = salarioBrutoFerias - inss;
-    let irpf = 0;
+    function calcularIRPF() {
 
-    if (baseIRPF <= 2259.20) {
-        irpf = 0;
-    } else if (baseIRPF <= 2826.65) {
-        irpf = (baseIRPF * 0.075) - 169.44;
-    } else if (baseIRPF <= 3751.05) {
-        irpf = (baseIRPF * 0.15) - 381.44;
-    } else if (baseIRPF <= 4664.68) {
-        irpf = (baseIRPF * 0.225) - 662.77;
-    } else {
-        irpf = (baseIRPF * 0.275) - 896.00;
+        let inss = calcularINSS();    
+        let baseIRPF = salarioBrutoFerias - inss;
+        let irpf = 0;
+
+        if (baseIRPF <= 2259.20) {
+            irpf = 0;
+        } else if (baseIRPF <= 2826.65) {
+            irpf = (baseIRPF * 0.075) - 169.44;
+        } else if (baseIRPF <= 3751.05) {
+            irpf = (baseIRPF * 0.15) - 381.44;
+        } else if (baseIRPF <= 4664.68) {
+            irpf = (baseIRPF * 0.225) - 662.77;
+        } else {
+            irpf = (baseIRPF * 0.275) - 896.00;
+        }
+
+        return irpf;
     }
 
     function feriasVencida() {
@@ -47,12 +57,12 @@ function calcularFerias() {
         };
     }
 
-    let feriasLiquida = ferias - inss - irpf;
+    let feriasLiquida = ferias - calcularINSS() - calcularIRPF();
 
     document.getElementById("vSalarioBrutoFerias").innerText = `R$ ${salarioBrutoFerias.toFixed(2)}`;
 
-    document.getElementById("dINSSFerias").innerHTML = `-R$ ${inss.toFixed(2)}`;
-    document.getElementById("dIRPFFerias").innerHTML = `-R$ ${irpf.toFixed(2)}`;
+    document.getElementById("dINSSFerias").innerHTML = `-R$ ${calcularINSS().toFixed(2)}`;
+    document.getElementById("dIRPFFerias").innerHTML = `-R$ ${calcularIRPF().toFixed(2)}`;
 
     document.getElementById("vMesesTrabalhados").innerText = `${mesesTrabalhado} meses`;
     document.getElementById("vStatusFerias").innerText = feriasVencida();
